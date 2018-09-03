9cd9e1bad2cb97ce0214e6b31150ce14d12e56f7
Tilman Dornheim <t.dornheim@student.maastrichtuniversity.nl>
2018-09-03 22:25:08 +0200

    Initial Commit

---
+++ app/src/main/java/com/example/tilman/samba_demo/mvp/home/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.tilman.samba_demo.mvp.home
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tilman.samba_demo.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
